f77b1dbdddf4ed69a9627c366ecb6dc65b9b8f44
nikolaiols <nikolaio@hiof.no>
2025-03-04 11:46:20 +0100

    mapper ut alle
    må finne ut hva jeg kan ha i key={} og bruke filter()

--- arbeidskrav_2/src/components/Resources.jsx
+++ arbeidskrav_2/src/components/Resources.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { resources } from "../components/ressurser.js"
 
 export default function Resources( {Category} ){
@@ -8,7 +9,18 @@ export default function Resources( {Category} ){
      {/*bruke filter for å skrive ut fra arrayen*/}
      {console.log("jeg funker", Category)}
     <h2>{Category}</h2>
-   
+      {resources.map(resource => (
+        <div key={resource.category}>
+          <h3>{resource.title}</h3>
+          <p>{resource.text}</p>
+          <ul>
+            {resource.sources.map(source => 
+                <li key={1}>
+                    <Link to={`${source.url}`}>{`${source.title}`}</Link>
+                </li>)}
+          </ul>
+        </div>
+      ))}
     </>
 
 );
